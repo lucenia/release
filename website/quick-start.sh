5be1a7c2af6690e791b557059ed1325ecf9142e9
Dylan Brophy <241793047+dbrophy5@users.noreply.github.com>
2026-06-04 23:54:49 +0000

    Add 0.11.1 to release manifests and set it as the latest version (#24)
    Signed-off-by: Dylan Brophy <dylan@lucenia.io>

--- website/quick-start.sh
+++ website/quick-start.sh
@@ -17,7 +17,7 @@ trap cleanup EXIT
 # Monitor process PID (global for cleanup)
 MONITOR_PID=""
 # Lucenia version defaults to 0.11.0 unless specified by environment variable
-LUCENIA_VERSION="${LUCENIA_VERSION:-0.11.0}"
+LUCENIA_VERSION="${LUCENIA_VERSION:-0.11.1}"
 LUCENIA_HOME="$HOME/.lucenia"
 LUCENIA_INITIAL_ADMIN_PASSWORD="myStrongPassword@123"
 LUCENIA_SEC_TOOLS="$LUCENIA_HOME/lucenia-$LUCENIA_VERSION/plugins/lucenia-security/tools"
